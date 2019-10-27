fa2bd45b9ddeee32cdd0aa97e6a02204e98ede05
Nat McConnaughay <natmcconnaughay@gmail.com>
2019-10-27 21:31:56 +0000

    Done.

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,29 @@
 class Waiter
-
+  attr_accessor :name, :experience
+  @@all =[]
+  
+  def initialize(name, experience)
+    @experience = experience
+    @name = name
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def new_meal(customer, total, tip)
+    Meal.new(self, customer, total, tip)
+  end
+  
+  def meals
+    Meal.all.select{|meal| meal.waiter == self}
+  end
+  
+  def best_tipper
+    tip_array = Customer.all.select{|customer| customer.waiters.include?(self)}
+    tip_array = tip_array.collect{|customer| customer.meals}.flatten
+    tip_array = tip_array.sort{|a,b| b.tip <=> a.tip}.first.customer
+  end
+  
 end
